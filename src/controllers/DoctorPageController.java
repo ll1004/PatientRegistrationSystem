f7b17e85983c79b738a8a6a7ed4d52cace615bb7
Hui Feng <huifeng001@gmail.com>
2023-11-22 15:53:17 -0500

    added doctor pages and functions

---
+++ src/controllers/DoctorPageController.java
@@ -0,0 +1,76 @@
+
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import models.PatientModel;
+
+public class PatientPageController implements Initializable {
+	// The title of current page
+	static final String TITLE = "Patient Home";
+	static final String FXM_URL = "/views/PatientPageView.fxml";
+	static final String CSS_URL = "../application/application.css";
+	static Parent root = null;
+	static Scene scene = null;
+	// The unique instance of the current controller to implement page switching
+	public static PatientPageController controller = null;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean initScene() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource(FXM_URL));
+		try {
+			root = loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	public boolean showScene() {
+		if (Main.stage == null || root == null) {
+			return false;
+		}
+		scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource(CSS_URL).toExternalForm());
+		Main.stage.setScene(scene);
+		Main.stage.setTitle(TITLE);
+		Main.stage.show();
+		return true;
+	}
+
+	public void logoutPatientPage() {
+		PatientModel.user = null;
+		PatientLoginController.controller.initScene();
+		PatientLoginController.controller.showScene();
+	}
+	
+	public void switchToViewRegistrationPage(){
+		PatientViewRegistrationController.controller.initScene();
+		PatientViewRegistrationController.controller.showScene();
+	}
+
+	public void switchToAddRegistrationPage() {
+		PatientAddRegistrationController.controller.initScene();
+		PatientAddRegistrationController.controller.showScene();
+	}
+
+	public void switchToManageProfilePage() {
+		PatientManageProfileController.controller.initScene();
+		PatientManageProfileController.controller.showScene();
+	}
+}
---
+++ src/models/DoctorModel.java
@@ -0,0 +1,162 @@
+package models;
+
+public class PatientModel {
+	private int id;
+	private String username;
+	private String password;
+	private int age;
+	private String gender;
+	private String phone;
+	private String email;
+	private String city;
+	private String state;
+	private String zipcode;
+	private String createTime;
+	private String updateTime;
+	//To save the unique instance of the currently logged in user
+	public static PatientModel user;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the zipcode
+	 */
+	public String getZipcode() {
+		return zipcode;
+	}
+	/**
+	 * @param zipcode the zipcode to set
+	 */
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	/**
+	 * @return the createTime
+	 */
+	public String getCreateTime() {
+		return createTime;
+	}
+	/**
+	 * @param createTime the createTime to set
+	 */
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	/**
+	 * @return the updateTime
+	 */
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	/**
+	 * @param updateTime the updateTime to set
+	 */
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+}
